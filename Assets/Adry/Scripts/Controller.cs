38e24b24675f9c33b09efd07dc0c4bdf15ae62b6
Adrytekk <adryan.hauton@gmail.com>
2021-12-18 06:37:18 +0100

    Character Controller

---
+++ Assets/Adry/Scripts/Controller.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public float MouvementSpeed = 10;
+    public float JumpHeight = 2;
+    public float gravity = -20f;
+    public bool isGrounded;
+
+    private Vector3 velocity; 
+
+    void FixedUpdate()
+    {
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * MouvementSpeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);;
+
+        if (Input.GetKey(KeyCode.Space) && controller.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(JumpHeight * -2f * gravity);
+        }
+
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+           controller.height = 1.5f;
+        }
+        else
+        {
+           controller.height = 2f;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+}
---
+++ Assets/Adry/Scripts/FpsCam.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FpsCam : MonoBehaviour
+{
+    public float maxSpeed = 500;
+    public float _mouseSpeed = 150;
+    public Transform playerBody;
+    private float xRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * _mouseSpeed * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * _mouseSpeed * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -65f, 65f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
